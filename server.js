32cf55c3c825735a5868c4c81fb023e5641e2dcf
ZikannSenpai <zidanriski778@gmail.com>
2026-03-10 17:55:09 +0700

    up

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, "index.html")));
+app.use(express.static(path.join(__dirname)));
 
 // simple rate limit
 const limiter = rateLimit({ windowMs: 60 * 1000, max: 60 });
